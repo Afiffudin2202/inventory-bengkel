f415324ee3f502dd43bdb18fea7ffe492633d767
Afiffudin2202 <afiffudin2222@gmail.com>
2024-02-29 22:20:34 +0700

    menyelesaikan halaman supplier

--- database/seeders/SupplierSeeder.php
+++ database/seeders/SupplierSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Supplier;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,18 @@ class SupplierSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+     
+        Supplier::create([
+            'kd_supplier' => 'S001',
+            'nama_supplier' => 'Honda Astra',
+            'alamat' => 'Cikarang',
+           
+        ]);
+        Supplier::create([
+            'kd_supplier' => 'S002',
+            'nama_supplier' => 'Yamaha ',
+            'alamat' => 'Karawang',
+           
+        ]);
     }
 }
